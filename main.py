a0cdab7c736f188ff9f5086f2de37685ef550db6
r-williamson <rwilliamson228@gmail.com>
2023-01-31 16:42:21 -0700

    added invoice date

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ for invoice in invoice_list:
     df = pd.read_excel(invoice, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    invoice_name = Path(invoice).stem
-    invoice_num = invoice_name.split("-")[0]
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_num}")
-    pdf.output(f"PDFs/{invoice_name}.pdf")
 
+    invoice_name = Path(invoice).stem
+    invoice_num, invoice_date = invoice_name.split("-")
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_num}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {invoice_date}")
+
+    pdf.output(f"PDFs/{invoice_name}.pdf")
 
 
