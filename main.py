f9cf32459fa68357a583e18640e061049330f41c
r-williamson <rwilliamson228@gmail.com>
2023-01-31 15:07:00 -0700

    load excel files into data frames

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from fpdf import FPDF
+import glob
 import pandas as pd
+from fpdf import FPDF
 
 
-invoice_list = []
+invoice_list = glob.glob("invoices/*xlsx*")
+
+for invoice in invoice_list:
+    df = pd.read_excel(invoice, sheet_name="Sheet 1")
+    print(df)
+
