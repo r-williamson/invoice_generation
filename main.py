cca36bded7a216415ea9dbcaa4427e275c81a336
r-williamson <rwilliamson228@gmail.com>
2023-01-31 17:02:44 -0700

    add a table

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ invoice_list = glob.glob("invoices/*xlsx*")
 
 
 for invoice in invoice_list:
-    df = pd.read_excel(invoice, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -18,7 +18,30 @@ for invoice in invoice_list:
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_num}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date: {invoice_date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {invoice_date}", ln=1)
+
+    df = pd.read_excel(invoice, sheet_name="Sheet 1")
+
+    # Add col headers
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", style="B", size=10)
+    # pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=str(columns[0]), border=1)
+    pdf.cell(w=70, h=8, txt=str(columns[1]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[2]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[3]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[4]), border=1, ln=1)
+
+    # Add rows to table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDFs/{invoice_name}.pdf")
 
