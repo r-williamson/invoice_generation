e33d2b7a612b86e01a3e874a47dba2a01d9a09cb
r-williamson <rwilliamson228@gmail.com>
2023-01-31 17:17:13 -0700

    added headers, cost summary, company name + logo

--- main.py
+++ main.py
@@ -43,6 +43,25 @@ for invoice in invoice_list:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    # Add total sum to bottom of table
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum as a sentence to bottom of PDF
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is: {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"RyanCorp")
+    pdf.image("logo.jpg", w=10)
+
     pdf.output(f"PDFs/{invoice_name}.pdf")
 
 
